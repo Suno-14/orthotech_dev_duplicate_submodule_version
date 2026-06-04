07f712a4678da5046cfba9930a88e9b46639b8ba
Suno-14 <sunosharjkr14@gmail.com>
2026-06-04 10:52:13 +0530

    Added ur_rtde for windows and linux

--- include/calculations.hpp
+++ include/calculations.hpp
@@ -2,6 +2,7 @@
 #define CALCULATIONS_HPP
 
 #include <Eigen/Eigenvalues>
+#include <ur_rtde/rtde_receive_interface.h>
 
 using namespace Eigen;
 class Calculations
@@ -12,6 +13,7 @@ public:
     double Multiplication(const double &a, const double &b);
     double Division(const double &a, const double &b);
     Matrix3d matrixMultiplication(Matrix3d mat1, Matrix3d mat2);
+    double get_timestamps();
 };
 
 #endif
--- src/calculations.cpp
+++ src/calculations.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <calculations.hpp>
 #include <Eigen/Eigenvalues>
+<<<<<<< HEAD
+=======
+// #include <ur_rtde/rtde_control_interface.h>
+#include <ur_rtde/rtde_receive_interface.h>
+// #include <ur_rtde/rtde_io_interface.h>
+>>>>>>> 6fe8166 (Added ur_rtde for windows and linux)
 
 using namespace std;
 using namespace Eigen;
 
+ur_rtde::RTDEReceiveInterface rtde_r;
+
 double Calculations::Addition(const double &n1, const double &n2)
-{return n1+n2;}
+    {return n1+n2;}
 
 double Calculations::Subtraction(const double &n1, const double &n2)
-{return n1-n2;}
+    {return n1-n2;}
 
 double Calculations::Multiplication(const double &n1, const double &n2)
-{return n1*n2;}
+    {return n1*n2;}
 
 double Calculations::Division(const double &n1, const double &n2)
-{
-if (n2==0) throw invalid_argument("Cannot divide by zero!");
-else return n1/n2;
-}
+    {
+    if (n2==0) throw invalid_argument("Cannot divide by zero!");
+    else return n1/n2;
+    }
 
 Matrix3d Calculations::matrixMultiplication(Matrix3d mat1, Matrix3d mat2)
-{
-    return mat1*mat2;
-}
+    {return mat1*mat2;}
+
+double Calculations::get_timestamps()
+    {return rtde_r.getTimestamp();}
+
 
